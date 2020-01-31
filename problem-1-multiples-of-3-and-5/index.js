b703a4780ee031d31e40a66ea006964cbe1b54aa
andyclarkdev <andyclark007@gmail.com>
2020-01-31 12:02:29 -0500

    Update index.js

--- problem-1-multiples-of-3-and-5/index.js
+++ problem-1-multiples-of-3-and-5/index.js
@@ -1,4 +1,9 @@
-function sumBelow(below) {
-  console.log('below: ', below);
-  document.getElementById('answer').innerHTML = 'answer';
+function sumBelow(below, sum, counter) {
+  if (counter >= below) return sum;
+  
+  const isMultiple = counter % 3 === 0 || counter % 5 === 0;
+  
+  sum = sum + (isMultiple || 0);
+  
+  return sumBelow(below, sum, counter++);
 }
