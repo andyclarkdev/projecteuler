ce01e629f6bc57a85816bdc7fd04997f1b503350
andyclarkdev <andyclark007@gmail.com>
2020-01-31 13:07:39 -0500

    Update index.js

--- problem-1-multiples-of-3-and-5/index.js
+++ problem-1-multiples-of-3-and-5/index.js
@@ -7,7 +7,7 @@ function sumMultiplesOfThreeAndFive(below, sum, counter) {
   //counter = counter > 0 ? counter : 0;
   //sum  = sum > 0 ? sum : 0;
   
-  const isMultiple = counter % 3 === 0 || counter % 5 === 0;
+  //const isMultiple = counter % 3 === 0 || counter % 5 === 0;
   
   return sumMultiplesOfThreeAndFive(below, sum + (counter % 3 === 0 || counter % 5 === 0 ? counter : 0), counter + 1);
 }
