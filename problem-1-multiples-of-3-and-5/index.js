4c6a98e8aa63a404e4092aadec9af95ff2090fb4
andyclarkdev <andyclark007@gmail.com>
2020-02-03 17:20:12 -0500

    Update index.js

--- problem-1-multiples-of-3-and-5/index.js
+++ problem-1-multiples-of-3-and-5/index.js
@@ -1,4 +1,7 @@
-function sumMultiplesOfThreeAndFive(below, sum, counter) {
+// need noprotect to prevent 'too much recursion' or 'possible infinite loop' errors/warnings
+// noprotect
+
+function recursiveSum(below, sum, counter) {
   if (counter >= below) {
     return sum;
   }
@@ -9,20 +12,54 @@ function sumMultiplesOfThreeAndFive(below, sum, counter) {
   
   const isMultiple = counter % 3 === 0 || counter % 5 === 0;
   
-  return sumMultiplesOfThreeAndFive(below, sum + (isMultiple ? counter : 0), counter + 1);
+  return recursiveSum(below, sum + (isMultiple ? counter : 0), counter + 1);
 }
 
-function sumBelow() {
-  let below = document.getElementById('below').value + 0;
-  below = below > 0 ? below : 0;
+
+function loopSum(below) {
+  //const arr = [];
   
+  //for (let counter = 1; counter < below; counter++) {
+    //if (counter % 3 === 0 || counter % 5 === 0) {
+      //arr.push(counter);
+    //}
+  //}
+  
+  //return arr.reduce((a,b) => a + b, 0);
+  let sum = 0;
+  
+  for (let counter = 1; counter < below; counter++) {
+    if (counter % 3 === 0 || counter % 5 === 0) {
+      sum = sum + counter;
+    }
+  }
+  
+  return sum;
+}
+
+function sum1() {
+  const below = document.getElementById('below1').value;
   const start = new Date();
   
-  const sum = sumMultiplesOfThreeAndFive(below);
+  const sum = recursiveSum(below, 0 , 1);
   
   const end = new Date();
   const diff = Math.abs(end - start) / 1000;
   
-  document.getElementById('answer').innerHTML = sum;
-  document.getElementById('timeToRun').innerHTML = diff;
+  document.getElementById('answer1').innerHTML = sum;
+  document.getElementById('timeToRun1').innerHTML = diff;
 }
+
+function sum2() {
+  const below = document.getElementById('below2').value;
+  const start = new Date();
+  
+  const sum = loopSum(below);
+  
+  const end = new Date();
+  const diff = Math.abs(end - start) / 1000;
+  
+  document.getElementById('answer2').innerHTML = sum;
+  document.getElementById('timeToRun2').innerHTML = diff;
+}
+
